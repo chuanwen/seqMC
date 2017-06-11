cdbab71225942d36e3d43f595cc66993fee7c086
Chuanwen Chen <chuanwen@gmail.com>
2017-06-10 22:41:11 -0700

    refactor: change k to t, K to T

--- R/seqMC.R
+++ R/seqMC.R
@@ -1,34 +1,35 @@
 #' Sequential Monte Carlo
 #'
-#' @param f function, when called with parameter k (time point) and 
-#' 		x_k (state vector at time k), it would return x_{k+1}
-#' @param prob_y_given_x function, when called with parameter k (time point), 
-#' 		y_k (observation vector at time k) and x_k (state vector at time k), it would return
-#' 		the conditional probability/density: Prob(y_k | x_k )
-#' @param x0 matrix, sample of state vector at time 0, one sample vector per col.
-#' @param y matrix, observations, col 1 is observation at time 1, col 2 is
-#'		observation at time 2, ... etc.
+#' @param f function, when called with parameter t (time point) and 
+#' 		x_t (state vector at time t), it would return x_(t+1)
+#' @param prob_y_given_x function, when called with parameter t (time point), 
+#' 		y_t (observation vector at time t) and x_t (state vector at time t), it would return
+#' 		the conditional probability/density: Prob(y_t | x_t )
+#' @param x0 matrix of N cols, sample of state vector at time 0, one sample vector per col,
+#'      N is the number of samples.
+#' @param y matrix of T cols, observations, col 1 is observation at time 1, col 2 is
+#'		observation at time 2, ... etc. T is the number of time points.
 #' @param sample_method character, specify sample method in the resample stage.
 #'      Default systematic, means "systematic resampling".
 #' @return sample from posterior distribution of state vectors, a 3D array, with
-#' 		dimension of d x N x K, where d is the length of a state vector, N is the number of samples,
-#'		K is the number of time steps.
+#' 		dimension of d x N x T, where d is the length of a state vector, N is the number of samples,
+#'		T is the number of time steps.
 #'
 #' @examples
 #'
-#' f <- function(k, x) {
-#'	 0.5 * x + 25 * x / (1 + x * x) + 8.0 * cos(1.2 * (k-1)) + rnorm(length(x), sd=sqrt(10.0))
+#' f <- function(t, x) {
+#'	 0.5 * x + 25 * x / (1 + x * x) + 8.0 * cos(1.2 * (t-1)) + rnorm(length(x), sd=sqrt(10.0))
 #' }
 #'
-#' prob_y_given_x <- function(k, y, x) {
+#' prob_y_given_x <- function(t, y, x) {
 #' 	 as.numeric(dnorm(y - x * x / 20.0))
 #' }
 #'
 #' ### simulate true path ###
-#' K = 50
-#' x = rep(0.0, K+1)
-#' for (k in 1:K) {
-#' 	 x[k+1] = f(k, x[k])
+#' T = 50
+#' x = rep(0.0, T+1)
+#' for (t in 1:T) {
+#' 	 x[t+1] = f(t, x[t])
 #' }
 #' x = x[-1]
 #' y = x * x / 20 + rnorm(length(x))
@@ -53,8 +54,8 @@ seqMC <- function(f, prob_y_given_x, x0, y,
 	              sample_method=c("systematic", "residual", "bootstrap")) {
 	stopifnot(is.matrix(x0), is.matrix(y))
 	sample_method = match.arg(sample_method)
-	N = ncol(x0)
-	K = ncol(y)
+	N = ncol(x0) # x0 is matrix of N cols, with N = number of samples (particles)
+	T = ncol(y)  # y is matrix of T cols, with T = number of time points.
 	
 	normalize <- function(x) {
 		x / sum(x)
@@ -86,17 +87,17 @@ seqMC <- function(f, prob_y_given_x, x0, y,
 	
 	resampler = get(sprintf("%s_sample", sample_method))
 	
-	x = list(K, mode="list")
-	for (k in 1:K) {
-		xk = f(k, x0)
-		q = normalize(prob_y_given_x(k, y[,k], xk))
-		stopifnot(is.matrix(xk), is.vector(q), ncol(xk) == length(q))
-		# resample cols of xk with probability q #
-		xk = xk[, resampler(q), drop=FALSE]
-		x[[k]] = xk 
-		x0 = xk
+	x = list(T, mode="list")
+	for (t in 1:T) {
+		xt = f(t, x0)
+		w = normalize(prob_y_given_x(t, y[,t], xt))
+		stopifnot(is.matrix(xt), is.vector(w), ncol(xt) == length(w))
+		# resample cols of xt with probability q #
+		xt = xt[, resampler(w), drop=FALSE]
+		x[[t]] = xt # save posterior distribution/sample at time t.
+		x0 = xt
 	}
 	x = unlist(x)
-	dim(x) = c(dim(x0), K)
+	dim(x) = c(dim(x0), T)
 	invisible(x)
 }
