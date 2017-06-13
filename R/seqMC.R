fc9eccff159e1458f5e4d5c6f48aec7d9837d60d
Chuanwen Chen <chuanwen@gmail.com>
2017-06-12 23:49:10 -0700

    1. add a parameter y0 to handle observation at time 0 (if has) 2. make normalize work even for all zero input (this is to handle the case where all particles have zero weight)

--- R/seqMC.R
+++ R/seqMC.R
@@ -1,5 +1,11 @@
 normalize <- function(x) {
-	x / sum(x)
+	sx = sum(x)
+	if (sx > 0) {
+		x / sx
+	} else {
+		warning("normalize alll zero vector")
+		rep(1.0/length(x), length(x))
+	}
 }
 
 bootstrap_sample <- function(prob, n=length(prob)) {
@@ -61,6 +67,7 @@ batchSeqMC <- function(f, prob_y_given_x, x0, y,
 #' 		y_t (observation vector at time t) and x_t (state vector at time t), it would return
 #' 		the conditional probability: Prob(y_t | x_t )
 #' @param x0 matrix, sample of state vector at time 0, each col is a sample of state at time 0.
+#' @param y0 observation at time 0 (can be missing).
 #' @param sample_method character, specify sample method in the resample stage.
 #'      Default systematic, means "systematic resampling".
 #' @return a seqMC object, which can be updated at each time point. e.g. \code{obj = update(ojb, y)}
@@ -101,11 +108,21 @@ batchSeqMC <- function(f, prob_y_given_x, x0, y,
 #' lines(xhat[3,], lty='dotted')
 #'
 #' @export
-seqMC <- function(f, prob_y_given_x, x0, sample_method=c("systematic", "residual", "bootstrap")) {
+seqMC <- function(f, prob_y_given_x, x0, y0, sample_method=c("systematic", "residual", "bootstrap")) {
 	stopifnot(is.matrix(x0)) # x0 is matrix of N cols, with N = number of samples (particles)
 	sample_method = match.arg(sample_method)
 	resampler = get(sprintf("%s_sample", sample_method))
-	structure(list(resampler=resampler, f=f, prob_y_given_x=prob_y_given_x, N=ncol(x0), t=0, x=x0), class="seqMC")
+	ans = structure(list(resampler=resampler,
+		                 f=f,
+						 prob_y_given_x=prob_y_given_x,
+						 N=ncol(x0),
+						 t=0,
+						 x=x0), class="seqMC")
+	if (!missing(y0)) {
+		resample.seqMC(ans, y0)
+	} else {
+		ans
+	}
 }
 
 
@@ -123,12 +140,22 @@ print.seqMC <- function(obj, ...) {
 #' @return seqMC object updated to next time point.
 #' @export
 update.seqMC <- function(obj, y) {
+	obj = next_time.seqMC(obj)
+	if(!missing(y)) {
+		obj = resample.seqMC(obj, y)
+	}
+	obj
+}
+
+next_time.seqMC <- function(obj) {
 	obj$x = obj$f(obj$t, obj$x)
 	obj$t = obj$t + 1
-	if(!missing(y)) {
-		w = normalize(obj$prob_y_given_x(obj$t, y, obj$x))
-		obj$x = obj$x[, obj$resampler(w), drop=FALSE]
-	}
+	obj
+}
+
+resample.seqMC <- function(obj, y) {
+	w = normalize(obj$prob_y_given_x(obj$t, y, obj$x))
+	obj$x = obj$x[, obj$resampler(w), drop=FALSE]
 	obj
 }
 
